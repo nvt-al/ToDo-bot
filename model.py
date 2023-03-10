892ff8eb40092447788d62a5b8942b02d0e5e7eb
ryzdo <denryzhankov@gmail.com>
2023-03-10 16:34:12 +0300

    add model.py and class Tasks

---
+++ model.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    owner = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Задача {} {}>'.format(self.id, self.name)
+
+# class lists(db.Model: Model):
+#     id = db.Column(db.Integer, primary_key=True)
