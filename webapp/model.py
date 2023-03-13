4169781cd1a75dcc1c0ab8d9a59aaa45f2b249e3
ryzdo <denryzhankov@gmail.com>
2023-03-13 18:18:18 +0300

    add models

--- webapp/model.py
+++ webapp/model.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 Base: model = db.Model
 
 
-class Tasks(Base):
+class TaskTemplates(Base):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     owner = db.Column(db.Integer, nullable=False)
@@ -15,5 +15,27 @@ class Tasks(Base):
         return "<Задача {} {}>".format(self.id, self.name)
 
 
-# class lists(db.Model: Model):
-#     id = db.Column(db.Integer, primary_key=True)
+class Statuses(Base):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "<Статус {} {}>".format(self.id, self.name)
+
+
+class Lists(Base):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "<Имя списка {}>".format(self.name)
+
+
+class ToDoList(Base):
+    id = db.Column(db.Integer, primary_key=True)
+    id_task = db.Column(db.Integer, nullable=False)
+    id_list = db.Column(db.Integer, nullable=False)
+    task_done = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return "<Имя списка {}>".format(self.name)
