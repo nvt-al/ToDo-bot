f95d3b32bb96be4ded31f6918a19ddc7c352319f
Alexander <nick.patrikov@yandex.ru>
2023-03-16 17:59:01 +0500

    full form v1.2

--- forms.py
+++ forms.py
@@ -1,18 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, DateTimeField, TimeField, SelectField
+from wtforms import (
+    StringField,
+    SubmitField,
+    DateField,
+    TimeField,
+    SelectField,
+)
 from wtforms.validators import DataRequired
 
 
 class TaskForm(FlaskForm):
-    new_task = StringField('Введите текст: ', validators=[DataRequired()])
-    description_task = StringField('Введите описание: ', validators=[DataRequired()])
-    to_time = TimeField('Введите время', format='%H:%M')
-    to_date= DateField('Введите дату', format='%H:%M:%S')
-    status = SelectField('Статус', choices=[('Выполнено', 'Выполнено'), ('Ожидается', 'Ожидается ')])
-    submit = SubmitField('Создать новое напоминание')
-
-    # submit_yes = SubmitField('Выполнено')
-    # submit_del = SubmitField('Отменить')
-
-
-    
+    new_task = StringField("Введите текст: ", validators=[DataRequired()])
+    description_task = StringField("Введите описание: ", validators=[DataRequired()])
+    to_time = TimeField("Введите время", format="%H:%M")
+    to_date = DateField("Введите дату", format="%H:%M:%S")
+    status = SelectField(
+        "Статус", choices=[("Выполнено", "Выполнено"), ("Ожидается", "Ожидается ")]
+    )
+    submit = SubmitField("Создать новое напоминание")
+    delete_task = SubmitField("Удалить")
+    edit_task = SubmitField("Редактировать")
