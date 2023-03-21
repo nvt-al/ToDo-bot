06dcd4014a92905085de8bffebf875c4ff3ed735
ryzdo <denryzhankov@gmail.com>
2023-03-21 11:09:50 +0300

    add user forms

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 
 from webapp.models import db
+from webapp.user.forms import LoginForm
 
 
 def create_app():
@@ -10,5 +11,11 @@ def create_app():
     app.config.from_pyfile("config.py")
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    @app.route("/login")
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template("login.html", page_title=title, form=login_form)
 
     return app
