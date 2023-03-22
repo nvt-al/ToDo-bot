9170967a0e82afadfc588551a17b564f120479c0
ryzdo <111901471+ryzdo@users.noreply.github.com>
2023-03-22 08:53:20 +0300

    Feature/users (#8)
    * add run-files
    
    * add migration user
    
    * add user forms
    
    * add login.html
    
    * add registration
    
    * fix registration
    
    * add SettingsForm
    
    * add settings.html
    
    * filling out the form settings
    
    * saving changes to settings

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from webapp.models import db
+from webapp.user.models import User
+from webapp.user.views import blueprint as user_blueprint
 
 
 def create_app():
@@ -10,5 +13,20 @@ def create_app():
     app.config.from_pyfile("config.py")
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    @app.route("/")
+    def index():
+        title = "Индекс"
+        return render_template("index.html", page_title=title)
+
+    app.register_blueprint(user_blueprint)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     return app
