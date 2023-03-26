215b5902b23bb2473e71e9f36ac83eaf2c7a7fab
ryzdo <denryzhankov@gmail.com>
2023-03-26 15:41:24 +0300

    added user authentication checks

--- webapp/APIv1/views.py
+++ webapp/APIv1/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, abort, jsonify, make_response, request
+from flask import Blueprint, abort, jsonify, make_response, request, url_for
 from flask_login import current_user
 
+from webapp.APIv1.decorators import login_required_API
 from webapp.models import Tasks, TaskTemplates, db
 
 blueprint = Blueprint("APIv1", __name__, url_prefix="/todo/api/v1.0/tasks")
@@ -11,7 +12,23 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
+@blueprint.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({"error": "Bad Request"}), 400)
+
+
+@blueprint.errorhandler(401)
+def unauthorized(error):
+    return make_response(jsonify({"error": "Unauthorized"}), 401)
+
+
+@blueprint.errorhandler(403)
+def forbidden(error):
+    return make_response(jsonify({"error": "Forbidden"}), 403)
+
+
 @blueprint.route("/", methods=["GET"])
+@login_required_API
 def get_tasks():
     query = (
         db.session.query(Tasks, TaskTemplates)
@@ -34,7 +51,8 @@ def get_tasks():
     for task in query:
         tasks.append(
             {
-                "task_id": task[0].id,
+                "uri": url_for("APIv1.get_task", task_id=task[0].id, _external=True),
+                "task_templates_id": task[1].id,
                 "name": task[1].name,
                 "description": task[1].description,
                 "task_done": task[0].task_done,
@@ -44,6 +62,7 @@ def get_tasks():
 
 
 @blueprint.route("/<int:task_id>", methods=["GET"])
+@login_required_API
 def get_task(task_id):
     query = (
         db.session.query(Tasks, TaskTemplates)
@@ -54,9 +73,12 @@ def get_task(task_id):
 
     if query is None:
         abort(404)
+    if query[1].owner != current_user.id:
+        abort(403)
 
     task = {
         "task_id": query[0].id,
+        "task_template_id": query[1].id,
         "name": query[1].name,
         "description": query[1].description,
         "is_active": query[1].is_active,
@@ -66,11 +88,20 @@ def get_task(task_id):
 
 
 @blueprint.route("/<int:task_id>", methods=["PUT"])
+@login_required_API
 def update_task(task_id):
     query = Tasks.query.filter_by(id=task_id).first()
+    query = (
+        db.session.query(Tasks, TaskTemplates)
+        .join(TaskTemplates, Tasks.id_task == TaskTemplates.id)
+        .filter(Tasks.id == task_id)
+        .first()
+    )
 
     if query is None:
         abort(404)
+    if query[1].owner != current_user.id:
+        abort(403)
     if not request.json:
         abort(400)
     if "task_done" in request.json and type(request.json["task_done"]) is not bool:
@@ -84,6 +115,7 @@ def update_task(task_id):
 
 
 @blueprint.route("/templates", methods=["GET"])
+@login_required_API
 def get_task_templates():
     task_templates = TaskTemplates.query.filter_by(owner=current_user.id).all()
     print([i.serialize for i in task_templates])
@@ -91,11 +123,14 @@ def get_task_templates():
 
 
 @blueprint.route("/templates/<int:task_template_id>", methods=["PUT"])
+@login_required_API
 def update_task_template(task_template_id):
     query = TaskTemplates.query.filter_by(id=task_template_id).first()
 
     if query is None:
         abort(404)
+    if query.owner != current_user.id:
+        abort(403)
     if not request.json:
         abort(400)
     if "is_active" in request.json and type(request.json["is_active"]) is not bool:
