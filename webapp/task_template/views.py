87155da5aa48d8d3846767d6bead54448fe7a95e
Denis Ryzhankov <denryzhankov@gmail.com>
2023-05-10 20:31:52 +0300

    added edit template

--- webapp/task_template/views.py
+++ webapp/task_template/views.py
@@ -28,7 +28,7 @@ def add_template():
     if not current_user.is_authenticated:
         return redirect(url_for("user.login"))
     form = TaskTemplateForm()
-    title = "Новая задач"
+    title = "Новая задача"
     return render_template("task_template/add.html", page_title=title, form=form)
 
 
@@ -47,12 +47,14 @@ def process_save_template():
 
         new_task = Task(date_id=current_user.active_date, task_template_id=new_task_template.id)
         db.session.add(new_task)
+
         if form.to_time.data:
             new_time = Reminder(
                 task_template_id=new_task_template.id, reminder_time=form.to_time.data
             )
             db.session.add(new_time)
         db.session.commit()
+
         logging.info(f"{new_task} created")
         if form.to_time.data:
             logging.info(f"{new_time} created")
@@ -62,3 +64,22 @@ def process_save_template():
             for error in errors:
                 flash('Ошибка в поле "{}": - {}'.format(getattr(form, field).label.text, error))
     return redirect(url_for("task_template.add_template"))
+
+
+@blueprint.route("/edit/<int:task_id>")
+@login_required
+def edit_template(task_id):
+    form = TaskTemplateForm()
+    title = f"Редактирование задачи {task_id}"
+    return render_template("task_template/edit.html", page_title=title, form=form)
+
+
+# @blueprint.route("/update/<int:task_id>", methods=["GET", "POST"])
+# @login_required
+# def update(task_id):
+#     task = TaskTemplate.query.filter_by(id=task_id).first()
+#     # if task.owner != current_user.id:
+#     #     abort(403)
+#     task.is_active = not task.is_active
+#     db.session.commit()
+#     return redirect(url_for("tasks.index"))
